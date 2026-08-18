9108e04ccd9545afe15fcf225467b806a8d92312
agguro <aguasguerreiroroberto@gmail.com>
2026-08-18 10:24:15 +0200

    update esp32c3_image.s

--- tools/x86_64/esp32c3_image/esp32c3_image.s
+++ tools/x86_64/esp32c3_image/esp32c3_image.s
@@ -6,6 +6,20 @@
 # Description: Pure x86_64 assembly tool to convert a raw binary (.bin)
 #              into a fully compliant ESP32-C3 boot image (adding 32-byte 
 #              header, padding to 16-byte boundary, and XOR checksum).
+#
+# Copyright 2026 agguro
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # =====================================================================
 
 .global _start
